502b275695291b541e24ef2c1fd35f2c5ccce11e
clementine delthe <clementine.delthe@gmail.com>
2023-03-07 16:12:54 +0100

    users method show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # if @user.family?
+    #   # action children#new
+    #   @child = Child.new
+    #   # action filitations#new
+    #   @filiation = Filiation.new
+    #   # action filiations#edit
+    #   @filiation.edit
+    # elsif @user.nanny?
+    #   @filiation.edit
+    # end
+  end
 end
