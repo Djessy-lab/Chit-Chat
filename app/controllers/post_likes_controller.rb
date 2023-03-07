19a8466410ce7e0af38d9a1dd79bd528e368c70f
nicolas riera <nicolariera@gmail.com>
2023-03-07 13:02:43 +0100

    create methods post like

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,2 +1,25 @@
 class PostLikesController < ApplicationController
+  before_action :set_post, only: %i[new create]
+
+  def create
+    @post_like = PostLike.new
+    @post_like.post = @post
+    if @post_like.save
+      redirect_to posts_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post_like = PostLike.find(params[:id])
+    @post_like.destroy
+    redirect_to posts_path(@post_like.post), status: :see_other
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post])
+  end
 end
