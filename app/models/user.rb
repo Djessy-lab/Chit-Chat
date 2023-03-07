2ffd68b643194e948bd30fa9950b907b82cde007
julie thomas <juliethomas1008@gmail.com>
2023-03-07 16:17:50 +0100

    post like finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :filiations, dependent: :destroy
   has_many :children, through: :filiations
   has_many :child_posts, through: :children
-  has_many :posts, through: :child_posts
+  has_many :my_posts, through: :child_posts, source: :post
+  has_many :posts
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true
