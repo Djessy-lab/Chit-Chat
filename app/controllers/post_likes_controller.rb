335eca33a10884bb3af35a8b05dbe5bfbebe451d
julie thomas <juliethomas1008@gmail.com>
2023-03-09 17:47:15 +0100

    turbo fait sur les likes des post

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -6,13 +6,26 @@ class PostLikesController < ApplicationController
     @post_like.post = @post
     @post_like.user = current_user
     @post_like.save
-    redirect_to posts_path
+    @comment_like = CommentLike.new
+    @post_like = PostLike.new
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.turbo_stream
+    end
   end
 
   def destroy
     @post_like = PostLike.find(params[:id])
+    @post = @post_like.post
     @post_like.destroy
-    redirect_to posts_path, status: :see_other
+    @comment_like = CommentLike.new
+    @post_like = PostLike.new
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { redirect_to posts_path, status: :see_other }
+      format.turbo_stream
+    end
   end
 
   private
