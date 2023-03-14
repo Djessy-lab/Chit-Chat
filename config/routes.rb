42466c1999352a035aca3a880a943b0623fe0145
julie thomas <juliethomas1008@gmail.com>
2023-03-14 14:20:52 +0100

    back des feedbacks côté parents fait

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get 'feedbacks', to: "pages#feedbacks"
 
+
   resources :posts, only: %i[index create update destroy], shallow: true do
     resources :comments, only: %i[create]
     resources :post_likes, only: %i[create destroy]
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   resources :children, only: %i[create destroy update], shallow: true do
     resources :filiations, only: %i[create update]
     resources :feedbacks, only: %i[index create update], shallow: false
+    get 'parent_feedbacks', to: 'feedbacks#parent_index'
   end
   resources :users, only: %i[index show update]
   resources :chatrooms, only: %i[index show new create destroy] do
