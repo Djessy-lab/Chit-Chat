64f037aa23636ad14886a015a67915032da06c15
nicolas riera <nicolariera@gmail.com>
2023-03-15 14:17:52 +0100

    navbar-scroll

--- app/javascript/controllers/navbar_hide_scrolldown_controller.js
+++ app/javascript/controllers/navbar_hide_scrolldown_controller.js
@@ -3,28 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar-hide-scrolldown"
 export default class extends Controller {
 
-  static targets = ["navbar-hide-scrolldown"]
+  static targets = ["navbar"]
 
-  const lastScrollTop
   updateNavbar() {
-    const navbar = document.querySelector(".navbar");
-    const scrollTop = window.scrollY
-    if (scrollTop > lastScrollTop) {
-      navbar.classList.add("navbar-hidden")
+    const currentScrollPos = window.scrollY
+    if (!this.prevScrollPos || this.prevScrollPos < currentScrollPos ) {
+      this.navbarTarget.classList.add("navbar-hidden")
     } else {
-      navbar.classList.remove("navbar-hidden")
+      this.navbarTarget.classList.remove("navbar-hidden")
     }
-    lastScrollTop = scrollTop;
-  }
+    this.prevScrollPos = currentScrollPos
+    }
 }
-
-// window.addEventListener('scroll',function(){
-//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   if(scrollTop > lastScrollTop){
-//   navbar.style.top='-80px';
-//   }
-//   else{
-//   navbar.style.top='0';
-//   }
-//   lastScrollTop = scrollTop;
-//   });
