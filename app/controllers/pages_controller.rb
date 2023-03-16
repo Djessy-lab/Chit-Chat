4e5b92735cc680529d546df3ab426a10933397c4
julie thomas <juliethomas1008@gmail.com>
2023-03-16 12:49:39 +0100

    affiliation Ok pour chat, notif et transmission

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def feedbacks
-    @children = current_user.children
+    @children = current_user.accepted_children
   end
 end
