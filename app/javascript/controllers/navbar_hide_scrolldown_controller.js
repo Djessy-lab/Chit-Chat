28b34eef9cbf3c076d6d76430aa4c7cf66117968
nicolas riera <nicolariera@gmail.com>
2023-03-16 09:13:13 +0100

    navar-en-attente

--- app/javascript/controllers/navbar_hide_scrolldown_controller.js
+++ app/javascript/controllers/navbar_hide_scrolldown_controller.js
@@ -6,12 +6,18 @@ export default class extends Controller {
   static targets = ["navbar"]
 
   updateNavbar() {
-    const currentScrollPos = window.scrollY
-    if (!this.prevScrollPos || this.prevScrollPos < currentScrollPos ) {
-      this.navbarTarget.classList.add("navbar-hidden")
-    } else {
-      this.navbarTarget.classList.remove("navbar-hidden")
-    }
-    this.prevScrollPos = currentScrollPos
-    }
+  //   // const currentScrollPos = window.scrollY
+  //   // if (!this.prevScrollPos || this.prevScrollPos < currentScrollPos ) {
+  //   //   this.navbarTarget.classList.add("navbar-hidden")
+  //   // } else {
+  //   //   this.navbarTarget.classList.remove("navbar-hidden")
+  //   // }
+  //   // this.prevScrollPos = currentScrollPos
+  //   // console.log(window.scrollY)
+  //   if (window.scrollY > 50) {
+  //     this.navbarTarget.classList.add("navbar-hidden")
+  //   } else {
+  //     this.navbarTarget.classList.remove("navbar-hidden")
+  //   }
+  }
 }
