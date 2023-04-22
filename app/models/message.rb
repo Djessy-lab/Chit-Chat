25dcd2fbd3e5fda65327534a48eb3f777c1379aa
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-04-22 10:48:17 +0200

    comment has_many notifications from message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
-  has_many :notifications, dependent: :destroy
+  # has_many :notifications, dependent: :destroy
 
   validates :content, presence: true
 end
